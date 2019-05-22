7b66f349974c54828085aa7e38fbb15f9096a286
Jasmine Walters <jsmnwltrs@gmail.com>
2019-05-21 21:28:53 -0500

    post order controller WIP

--- SkullAndDaisy/Controllers/OrdersController.cs
+++ SkullAndDaisy/Controllers/OrdersController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SkullAndDaisy.Data;
+using SkullAndDaisy.Models;
 
 namespace SkullAndDaisy.Controllers
 {
@@ -11,6 +13,12 @@ namespace SkullAndDaisy.Controllers
     [ApiController]
     public class OrdersController : ControllerBase
     {
+        [HttpPost("addOrder")]
+        public ActionResult CreateOrder(Order orderObject)
+        {
+            var newOrder = OrderRepository.AddOrder(orderObject.OrderStatus, orderObject.Total, orderObject.OrderDate, orderObject.PaymentTypeId, orderObject.UserId);
 
+            return Created($"api/createdOrder/{newOrder.Id}", newOrder);
+        }
     }
 }
--- SkullAndDaisy/Data/OrderRepository.cs
+++ SkullAndDaisy/Data/OrderRepository.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SkullAndDaisy.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,17 @@ namespace SkullAndDaisy.Data
 {
     public class OrderRepository
     {
+
+        public static List<Order> _orders = new List<Order>();
+
+        const string ConnectionString = "Server = localhost; Database = SkullAndDaisy; Trusted_Connection = True;";
+
+        static Order AddOrder(string orderStatus, decimal total, DateTime orderDate, int paymentTypeId, int userId)
+        {
+            using(var db = new SqlConnection(ConnectionString))
+            {
+                var newOrderObject = 
+            }
+        }
     }
 }
