bc88a31d04529eeec892c3355a9204b7ea0b8436
Jasmine Walters <jsmnwltrs@gmail.com>
2019-05-21 20:36:52 -0500

    setup order and product order models

---
+++ SkullAndDaisy/Models/ProductOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkullAndDaisy.Models
+{
+    public class ProductOrder
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int OrderId { get; set;
+        }
+    }
+}
