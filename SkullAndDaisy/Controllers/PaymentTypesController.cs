85dd76af7a81fa0bcb21e4849c3d2e37ecc11486
unknown <marshalloffutt@gmail.com>
2019-05-26 10:57:40 -0500

    adds get all payment types for user

---
+++ SkullAndDaisy/Controllers/PaymentTypesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SkullAndDaisy.Models;
+using SkullAndDaisy.Data;
+
+namespace SkullAndDaisy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentTypesController : ControllerBase
+    {
+        readonly PaymentTypeRepository _paymentTypeRepository;
+
+        public PaymentTypesController()
+        {
+            _paymentTypeRepository = new PaymentTypeRepository();
+        }
+
+        [HttpGet("{userId}")]
+        public ActionResult GetAllPaymentTypesForUser(int userId)
+        {
+            var paymentTypes = _paymentTypeRepository.GetAllPaymentTypesForUser(userId);
+
+            return Ok(paymentTypes);
+        }
+    }
+}
