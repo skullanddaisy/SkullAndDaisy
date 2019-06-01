1d990d882496607c4b5a16386474ccca1b07051d
unknown <marshalloffutt@gmail.com>
2019-06-01 10:47:29 -0500

    adds dependency injection, setup auth

--- SkullAndDaisy/Startup.cs
+++ SkullAndDaisy/Startup.cs
@@ -1,3 +1,5 @@
+using SkullAndDaisy.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -5,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace SkullAndDaisy
 {
@@ -21,6 +24,30 @@ namespace SkullAndDaisy
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.IncludeErrorDetails = true;
+                    options.Authority = "https://securetoken.google.com/skull-and-daisy";
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = "https://securetoken.google.com/skull-and-daisy",
+                        ValidateAudience = true,
+                        ValidAudience = "skull-and-daisy",
+                        ValidateLifetime = true
+                    };
+                });
+
+            services.Configure<DbConfiguration>(Configuration);
+            services.AddTransient<UserRepository>();
+            services.AddTransient<OrderRepository>();
+            services.AddTransient<PaymentTypeRepository>();
+            services.AddTransient<ProductOrderRepository>();
+            services.AddTransient<ProductRepository>();
+            services.AddTransient<ProductTypeRepository>();
+
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -43,15 +70,11 @@ namespace SkullAndDaisy
             }
 
             app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
 
             app.UseSpa(spa =>
             {
@@ -64,4 +87,11 @@ namespace SkullAndDaisy
             });
         }
     }
+
+    public class DbConfiguration
+    {
+        public string ConnectionString { get; set; }
+    }
 }
+
+
