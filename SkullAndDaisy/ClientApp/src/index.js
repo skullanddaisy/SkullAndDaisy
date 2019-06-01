5b26658e94e6e794b4dba4b9adba683c694bb30f
unknown <marshalloffutt@gmail.com>
2019-06-01 12:23:53 -0500

    setup frontend

--- SkullAndDaisy/ClientApp/src/index.js
+++ SkullAndDaisy/ClientApp/src/index.js
@@ -1,11 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/index.scss';
+import App from './App/App';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
