c747959122844b424a41acb9a132aa392cca6973
unknown <marshalloffutt@gmail.com>
2019-06-08 12:30:55 -0500

    scaffolds out account management page

--- SkullAndDaisy/ClientApp/src/components/pages/UserAccount/UserAccount.js
+++ SkullAndDaisy/ClientApp/src/components/pages/UserAccount/UserAccount.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import OrderHistory from './OrderHistory/OrderHistory';
+import SalesHistory from './SalesHistory/SalesHistory';
 import PaymentMethod from './PaymentMethod/PaymentMethod';
 import LoginSettings from './LoginSettings/LoginSettings';
 
@@ -8,15 +9,32 @@ import './UserAccount.scss';
 class UserAccount extends React.Component {
     render() {
         return (
-            <div className='top-row'>
-                <div className="your-orders">
-                    <OrderHistory />
+            <div className='user-account'>
+                <h1>Account Management</h1>
+
+                <div className="user-account-container mt-5">
+
+                    <div className="user-account-section">
+                        <div className="user-account-sub-section">
+                            <OrderHistory />
+                        </div>
+
+                        <div className="user-account-sub-section">
+                            <SalesHistory />
+                        </div>
+                    </div>
+
+                    <div className="user-account-section">
+                        <div className="user-account-sub-section">
+                            <PaymentMethod />
+                        </div>
+                        <div className="user-account-sub-section">
+                            <LoginSettings />
+                        </div>
+                    </div>
                 </div>
-                <div className="bottom-row">
-                    <PaymentMethod />
-                    <LoginSettings />
-                </div>
-            </div>
+
+            </div >
         );
     }
 }
