57a0be5f19ff1bcbf97200bb27f847a2ed4bace4
unknown <marshalloffutt@gmail.com>
2019-06-08 12:56:58 -0500

    account management launchpad

--- SkullAndDaisy/ClientApp/src/components/pages/UserAccount/UserAccount.js
+++ SkullAndDaisy/ClientApp/src/components/pages/UserAccount/UserAccount.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import OrderHistory from './OrderHistory/OrderHistory';
-import SalesHistory from './SalesHistory/SalesHistory';
-import PaymentMethod from './PaymentMethod/PaymentMethod';
-import LoginSettings from './LoginSettings/LoginSettings';
 
 import './UserAccount.scss';
 
@@ -10,30 +6,44 @@ class UserAccount extends React.Component {
     render() {
         return (
             <div className='user-account'>
+
                 <h1>Account Management</h1>
 
-                <div className="user-account-container mt-5">
+                <div className="user-account-container mx-auto">
 
-                    <div className="user-account-section">
-                        <div className="user-account-sub-section">
-                            <OrderHistory />
+                    <div className="card-deck mt-5 row">
+
+                        <div className="card border-dark" id="orders" onClick={this.changeView}>
+                            <div className="card-body text-center">
+                                <h4 className="card-title"><i className="fas fa-box-open fa-6x"></i></h4>
+                                <h6 className="card-subtitle mb-2 text-muted">Orders</h6>
+                                <p className="card-text">See your order history</p>
+                            </div>
+                        </div>
+                        <div className="card border-dark" id='sales' onClick={this.changeView}>
+                            <div className="card-body text-center">
+                                <h4 className="card-title"><i className="fas fa-hand-holding-usd fa-6x"></i></h4>
+                                <h6 className="card-subtitle mb-2 text-muted">Sales</h6>
+                                <p className="card-text">Seller Management</p>
+                            </div>
+                        </div>
+                        <div className="card border-dark" id='payment' onClick={this.changeView}>
+                            <div className="card-body text-center">
+                                <h4 className="card-title"><i className="far fa-credit-card fa-6x"></i></h4>
+                                <h6 className="card-subtitle mb-2 text-muted">Payment Methods</h6>
+                                <p className="card-text">Manage your payment</p>
+                            </div>
+                        </div>
+                        <div className="card border-dark" id='login' onClick={this.changeView}>
+                            <div className="card-body text-center">
+                                <h4 className="card-title"><i className="fas fa-user-circle fa-6x"></i></h4>
+                                <h6 className="card-subtitle mb-2 text-muted">Login Settings</h6>
+                                <p className="card-text">Change username, password, etc</p>
+                            </div>
                         </div>
 
-                        <div className="user-account-sub-section">
-                            <SalesHistory />
-                        </div>
-                    </div>
-
-                    <div className="user-account-section">
-                        <div className="user-account-sub-section">
-                            <PaymentMethod />
-                        </div>
-                        <div className="user-account-sub-section">
-                            <LoginSettings />
-                        </div>
                     </div>
                 </div>
-
             </div >
         );
     }
