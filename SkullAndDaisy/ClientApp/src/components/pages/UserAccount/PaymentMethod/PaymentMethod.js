97c44fa93458b633b62cba873f6e701248c1bbe0
unknown <marshalloffutt@gmail.com>
2019-06-08 10:01:43 -0500

    Scaffolds account profile

---
+++ SkullAndDaisy/ClientApp/src/components/pages/UserAccount/PaymentMethod/PaymentMethod.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './PaymentMethod.scss';
+
+class PaymentMethod extends React.Component {
+    render() {
+        return (
+            <h1>PaymentMethod</h1>
+        );
+    }
+}
+
+export default PaymentMethod;
---
+++ SkullAndDaisy/ClientApp/src/components/pages/UserAccount/UserAccount.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import OrderHistory from './OrderHistory/OrderHistory';
+import PaymentMethod from './PaymentMethod/PaymentMethod';
+import LoginSettings from './LoginSettings/LoginSettings';
+
+import './UserAccount.scss';
+
+class UserAccount extends React.Component {
+    render() {
+        return (
+            <div className = 'top-row'>
+                <div className="your-orders">
+                    <OrderHistory />
+                </div>
+                <div className="bottom-row">
+                    <PaymentMethod />
+                    <LoginSettings />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
