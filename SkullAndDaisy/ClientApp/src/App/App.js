f0910ef2fa3e0541539ee547872778d1278ee4fa
Jasmine Walters <jsmnwltrs@gmail.com>
2019-06-10 12:02:53 -0500

    fixes

--- SkullAndDaisy/ClientApp/src/App/App.js
+++ SkullAndDaisy/ClientApp/src/App/App.js
@@ -33,11 +33,7 @@ const PublicRoute = ({ component: Component, authed, ...rest }) => {
   return <Route {...rest} render={props => routeChecker(props)} />;
 };
 
-const PrivateRoute = ({
-  component: Component,
-  authed,
-  ...rest
-}) => {
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
   const routeChecker = props => (authed === true
     ? (<Component {...props } />)
     : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
