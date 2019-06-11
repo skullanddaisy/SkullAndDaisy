bf88edf8ca25beae24f6a55c8de12e51b2a395ab
unknown <marshalloffutt@gmail.com>
2019-06-10 21:32:42 -0500

    adds profileshape

--- SkullAndDaisy/ClientApp/src/components/pages/LoginSettings/LoginSettings.js
+++ SkullAndDaisy/ClientApp/src/components/pages/LoginSettings/LoginSettings.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import userRequests from '../../../helpers/data/userRequests';
 import './LoginSettings.scss';
 
 class LoginSettings extends React.Component {
+  state = {
+    userId: 0,
+  }
+
+  componentDidMount() {
+    userRequests.getUserIdByEmail()
+      .then((userId) => {
+        this.setState({ userId });
+      }).catch((error) => {
+        console.error(error);
+      });
+  }
+
+
   render() {
     return (
       <div className='login-settings'>
