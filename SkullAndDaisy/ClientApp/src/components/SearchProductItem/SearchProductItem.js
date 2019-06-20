bd59bda98b471de724f1553c6d36c14ddfdc9b21
Austin <austincumberlander@gmail.com>
2019-06-19 20:16:46 -0500

    finished searchBar and fixed refresh routing issue!

--- SkullAndDaisy/ClientApp/src/components/SearchProductItem/SearchProductItem.js
+++ SkullAndDaisy/ClientApp/src/components/SearchProductItem/SearchProductItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import productShape from '../../helpers/props/productShape';
 import formatPrice from '../../helpers/formatPrice';
 import formatProductType from '../../helpers/formatProductType';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 import './SearchProductItem.scss';
 
@@ -10,17 +10,18 @@ export default class SearchProductItem extends React.Component {
   static propTypes = {
     product: productShape,
   }
-
-  goToDetailsPage = () => {
-	  const productDetails = `productdetails/${this.props.product.id}`
-	  this.state.history.push(productDetails)
-  }
-
+  
+  
   render() {
-    const { product } = this.props;
+	  const { product } = this.props;
+	  
+	  const goToDetailsPage = () => {
+		const productDetails = `productdetails/${product.id}`;
+		window.location.assign(productDetails);
+	  }
 
 	return (
-		<tr className="searchTableItem" onClick={this.goToDetailsPage}>
+		<tr className="searchTableItem" onClick={goToDetailsPage}>
 			<tb><img className='searchImage' src={product.imageUrl} alt='this is a product' /></tb>
 			<th><div className=''>{product.title}</div>{product.description}</th>
 			<td>{formatProductType(product.productTypeId)}</td>
