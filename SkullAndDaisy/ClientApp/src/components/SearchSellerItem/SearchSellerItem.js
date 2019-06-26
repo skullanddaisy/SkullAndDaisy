54e9e3f630406e0e87d2ae6f0fe7bd14bddb7cd6
unknown <marshalloffutt@gmail.com>
2019-06-25 19:19:14 -0500

    finishes seller search on navbar

--- SkullAndDaisy/ClientApp/src/components/SearchSellerItem/SearchSellerItem.js
+++ SkullAndDaisy/ClientApp/src/components/SearchSellerItem/SearchSellerItem.js
@@ -7,13 +7,13 @@ import './SearchSellerItem.scss';
 export default class SearchSellerItem extends React.Component {
   render() {
     const { seller } = this.props;
-    // const goToDetailsPage = () => {
-    //   const productDetails = `sellerDetails/${seller.id}`;
-    //   window.location.assign(sellerDetails);
-    // };
+    const goToSellerStore = () => {
+      const sellerStore = `sellerstore/${seller.id}`;
+      window.location.assign(sellerStore);
+    };
 
     return (
-			<tr className="searchTableItem">
+			<tr className="searchTableItem" onClick={goToSellerStore}>
         <td> </td>
 				<td>{seller.firstName} {seller.lastName}</td>
 				<td>{seller.username}</td>
