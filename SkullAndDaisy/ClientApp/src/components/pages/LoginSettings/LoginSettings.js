bfd0ccee442e2b3609389251f8a2b1e9144261b9
unknown <marshalloffutt@gmail.com>
2019-06-29 10:30:21 -0500

    change edit user modal to modals

--- SkullAndDaisy/ClientApp/src/components/pages/LoginSettings/LoginSettings.js
+++ SkullAndDaisy/ClientApp/src/components/pages/LoginSettings/LoginSettings.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Button } from 'reactstrap';
+import EditUserNameModal from '../../Modals/EditUsernameModal';
 import userRequests from '../../../helpers/data/userRequests';
-import LoginSettingsModal from '../../LoginSettingsModal/LoginSettingsModal';
 import './LoginSettings.scss';
 
 class LoginSettings extends React.Component {
@@ -46,14 +47,51 @@ class LoginSettings extends React.Component {
     return (
       <div className='login-settings'>
         <h1>Login Settings</h1>
-        <div class="card text-white bg-dark profile-card mt-4">
-          <div class="card-body">
-            <h4 class="card-title">Username: {currentUser.username}</h4>
-            <h5 class="card-title">First Name: {currentUser.firstName}</h5>
-            <h5 class="card-title">Last Name: {currentUser.lastName}</h5>
-            <p class="card-text">Email: {currentUser.email}</p>
-            <LoginSettingsModal buttonLabel="Edit" currentUser={currentUser} onSubmit={this.formSubmitEvent}/>
+        <div class="card tborder-dark mb-3 profile-card mt-4">
+          <div class="card-body card-header p-3 d-flex justify-content-between">
+            <div class="d-flex flex-column text-left">
+              <p class="card-title font-weight-bold">Username:</p>
+              <p>{currentUser.username}</p>
+            </div>
+            <div class="m-3 ml-5">
+              <EditUserNameModal
+                buttonLabel='Edit'
+                currentUser={currentUser}
+                onSubmit={this.formSubmitEvent}
+              />
+            </div>
           </div>
+
+          <div class="card-body card-header p-3 d-flex justify-content-between">
+            <div class="d-flex flex-column text-left">
+              <p class="card-title font-weight-bold">Name:</p>
+              <p>{currentUser.firstName} {currentUser.lastName}</p>
+            </div>
+            <div class="m-3 ml-5">
+              <Button color="secondary">Edit</Button>
+            </div>
+          </div>
+
+          <div class="card-body card-header p-3 d-flex justify-content-between">
+            <div class="d-flex flex-column text-left">
+              <p class="card-title font-weight-bold">Email:</p>
+              <p>{currentUser.email}</p>
+            </div>
+            <div class="m-3 ml-5">
+              <Button color="secondary">Edit</Button>
+            </div>
+          </div>
+
+          <div class="card-body card-header p-3 d-flex justify-content-between">
+            <div class="d-flex flex-column text-left">
+              <p class="card-title font-weight-bold">Password:</p>
+              <p>********</p>
+            </div>
+            <div class="m-3 ml-5">
+              <Button color="secondary">Edit</Button>
+            </div>
+          </div>
+
         </div>
       </div>
     );
--- SkullAndDaisy/ClientApp/src/helpers/data/authRequests.js
+++ SkullAndDaisy/ClientApp/src/helpers/data/authRequests.js
@@ -1,56 +1,81 @@
-﻿import firebase from 'firebase';
+﻿/* eslint-disable arrow-body-style */
+/* eslint-disable arrow-parens */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable func-names */
+import firebase from 'firebase';
 import axios from 'axios';
 
 axios.interceptors.request.use(function (request) {
   const token = sessionStorage.getItem('token');
   if (token != null) {
-        request.headers.Authorization = `Bearer ${token}`;
-    }
+    request.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return request;
+  return request;
 }, function (err) {
-    return Promise.reject(err);
+  return Promise.reject(err);
 });
 
 axios.interceptors.response.use((response) => {
   return response;
 }, errorResponse => {
-    console.error("Blew up")
+  console.error('Blew up');
 });
 
 const registerUser = (user) => {
-    return firebase.auth().createUserWithEmailAndPassword(user.email, user.password);
+  return firebase.auth().createUserWithEmailAndPassword(user.email, user.password);
 };
 
 const loginUser = (user) => {
-    return firebase.auth().signInWithEmailAndPassword(user.email, user.password).then(cred => {
-        getCurrentUserJwt();
-    });
+  return firebase.auth().signInWithEmailAndPassword(user.email, user.password).then(cred => {
+    getCurrentUserJwt();
+  });
+};
+
+const getOldEmail = () => {
+  return firebase.auth().currentUser.email;
+};
+
+const getOldPassword = () => {
+  return firebase.auth().currentUser.password;
+};
+
+const makeCredential = (oldEmail, oldPassword) => {
+  return firebase.auth.EmailAuthProvider.credential(oldEmail, oldPassword);
+};
+
+const updateEmailAndPassword = (credential, newEmail, newPassword) => {
+  firebase.auth().currentUser.reauthenticateWithCredential(credential).then(() => {
+    firebase.auth().currentUser.updatePassword(newPassword);
+    firebase.auth().currentUser.updateEmail(newEmail);
+  });
 };
 
 const logoutUser = () => {
-    return firebase.auth().signOut();
+  return firebase.auth().signOut();
 };
 
 const getUid = () => {
-    return firebase.auth().currentUser.uid;
+  return firebase.auth().currentUser.uid;
 };
 
-const getUserEmail = () =>{
-    return firebase.auth().currentUser.email;
+const getUserEmail = () => {
+  return firebase.auth().currentUser.email;
 };
 
 
 const getCurrentUserJwt = () => firebase
-    .auth()
-    .currentUser.getIdToken()
-    .then(token => sessionStorage.setItem('token', token));
+  .auth()
+  .currentUser.getIdToken()
+  .then(token => sessionStorage.setItem('token', token));
 
 export default {
-    getUid,
-    getUserEmail,
-    loginUser,
-    logoutUser,
-    registerUser,
-    getCurrentUserJwt,
+  getUid,
+  getUserEmail,
+  loginUser,
+  logoutUser,
+  registerUser,
+  getCurrentUserJwt,
+  makeCredential,
+  updateEmailAndPassword,
 };
