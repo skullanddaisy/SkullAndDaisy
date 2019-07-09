a61251ec1fa17814a88898df2ffa5feba6e9edad
Austin <austincumberlander@gmail.com>
2019-07-09 11:43:54 -0500

    deal of the day component is complete. It randomizes the pictures shown and has a link that routes to the appropriate product

--- SkullAndDaisy/ClientApp/src/components/pages/Home/DealOfTheDayCard/DealOfTheDayCard.js
+++ SkullAndDaisy/ClientApp/src/components/pages/Home/DealOfTheDayCard/DealOfTheDayCard.js
@@ -1,14 +1,42 @@
 import React from 'react';
+import poisonPic from '../../../../img/knockemdead.png';
+import potionPic from '../../../../img/healingTouch.png';
+import herbPic from '../../../../img/naturalRemedy.png';
+import crystalPic from '../../../../img/allThatGlitters.png';
+import { Link } from 'react-router-dom';
 import './DealOfTheDayCard.scss';
 
-class DealOfTheDayCard extends React.Component {
+const picArray = [poisonPic, potionPic, herbPic, crystalPic];
+let routeToPage = '';
+
+const dealOfTheDay = (productPics) => {
+	const productSelector = Math.floor(Math.random() * productPics.length);
+  if (productPics[productSelector] == poisonPic) {
+    routeToPage = '/productdetails/9';
+  }
+  if (productPics[productSelector] == potionPic) {
+    routeToPage = '/productdetails/30';
+  }
+  if (productPics[productSelector] == herbPic) {
+    routeToPage = '/productdetails/14';
+  }
+  if (productPics[productSelector] == crystalPic) {
+    routeToPage = '/productdetails/17';
+  }
+  return  <Link to={routeToPage}>
+            <img className="dealImagePic" src={productPics[productSelector]} alt="deal of the day"></img>
+          </Link>
+  // return productPics[productSelector];
+  };
+
+export default class DealOfTheDayCard extends React.Component {
   render() {
     return (
       <div className='deal-of-the-day-card'>
         <h2>Deal Of The Day Component</h2>
+        {dealOfTheDay(picArray)}
       </div>
     );
   }
 }
 
-export default DealOfTheDayCard;
